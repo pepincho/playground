a769dfae10914e2481b1123ecc9e36eb8fc43c81
Petar Ivanov <pepincho95@gmail.com>
2018-01-04 14:32:45 +0200

    Add StartUp timeout

--- reactNavigation/src/screens/StartUpScreen.js
+++ reactNavigation/src/screens/StartUpScreen.js
@@ -5,10 +5,29 @@ import {
   Text,
   View
 } from 'react-native';
+import { StackNavigator, NavigationActions } from 'react-navigation';
+
+
+import SignInScreen from './SignInScreen';
 
 
 export default class StartUpScreen extends Component<{}> {
   render() {
+
+    // const screen = SignInScreen;
+    // const params = null;
+    // const path = null;
+    // const { router } = screen;
+    // const action = path && router.getActionForPathAndParams(path, params);
+
+    setTimeout(
+      () => {
+        this.props.navigation.navigate('SignInScreen', {}, null);
+        // this.props.navigation.navigate('MainServiceScreen');
+        // this.props.navigation.navigate('StartBirthdayScreen');
+      }, 1000
+    );
+
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>

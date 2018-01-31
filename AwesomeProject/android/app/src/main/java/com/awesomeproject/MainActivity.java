9202a3dab880ff789e5fb96b259c3e0c2503cb49
Petar Ivanov <pepincho95@gmail.com>
2018-01-31 15:40:13 +0200

    Add react-native-calendar-events

--- AwesomeProject/android/app/src/main/java/com/awesomeproject/MainActivity.java
+++ AwesomeProject/android/app/src/main/java/com/awesomeproject/MainActivity.java
@@ -4,12 +4,21 @@ import com.facebook.react.ReactActivity;
 
 import android.content.Intent;
 
+import com.calendarevents.CalendarEventsPackage;
+
+
 public class MainActivity extends ReactActivity {
 
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
      */
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        CalendarEventsPackage.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
--- AwesomeProject/android/app/src/main/java/com/awesomeproject/MainApplication.java
+++ AwesomeProject/android/app/src/main/java/com/awesomeproject/MainApplication.java
@@ -3,6 +3,7 @@ package com.awesomeproject;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.calendarevents.CalendarEventsPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
@@ -39,6 +40,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new CalendarEventsPackage(),
             new MapsPackage(),
             new LinearGradientPackage(),
             new VectorIconsPackage(),
